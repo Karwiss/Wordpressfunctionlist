83b685653459d4f8072f6f53c33a0e900ebe021d
Karwiss <100564647+Karwiss@users.noreply.github.com>
2023-05-04 10:44:32 +0530

    wordpressfunctionlist

---
+++ wordpressfunctionlist.php
@@ -0,0 +1,49 @@
+<?php
+/*
+It would be difficult to list the top 100 functions in WordPress as there are many functions that can be considered important depending on the use case. However, I can provide a list of some commonly used functions in WordPress:
+
+wp_head(): Outputs scripts and styles in the head section of the website.
+wp_footer(): Outputs scripts and styles in the footer section of the website.
+get_header(): Retrieves the header template of the current theme.
+get_footer(): Retrieves the footer template of the current theme.
+get_sidebar(): Retrieves the sidebar template of the current theme.
+the_title(): Displays the title of a post or page.
+the_content(): Displays the content of a post or page.
+the_excerpt(): Displays the excerpt of a post or page.
+get_the_post_thumbnail(): Retrieves the featured image of a post or page.
+the_permalink(): Displays the permalink of a post or page.
+the_author(): Displays the author name of a post or page.
+the_category(): Displays the category of a post or page.
+the_time(): Displays the time of a post or page.
+the_tags(): Displays the tags of a post or page.
+get_post_meta(): Retrieves metadata for a post or page.
+wp_list_categories(): Displays a list of categories on the website.
+wp_list_pages(): Displays a list of pages on the website.
+get_search_form(): Retrieves the search form template of the current theme.
+wp_enqueue_script(): Registers and enqueues a script for use on the website.
+wp_enqueue_style(): Registers and enqueues a stylesheet for use on the website.
+wp_register_script(): Registers a script for use on the website.
+wp_register_style(): Registers a stylesheet for use on the website.
+wp_deregister_script(): Removes a registered script from the website.
+wp_deregister_style(): Removes a registered stylesheet from the website.
+wp_enqueue_media(): Enqueues the WordPress media scripts and styles.
+wp_editor(): Displays a WordPress editor for user input.
+wp_redirect(): Redirects the user to a specified URL.
+get_bloginfo(): Retrieves information about the website.
+get_permalink(): Retrieves the permalink of a post or page.
+get_post(): Retrieves a post object for a specified post.
+get_posts(): Retrieves an array of post objects based on specified criteria.
+get_template_directory_uri(): Retrieves the URI of the current theme directory.
+get_template_part(): Retrieves and includes a template file from the current theme.
+is_home(): Determines if the current page is the homepage.
+is_front_page(): Determines if the current page is the front page.
+is_page(): Determines if the current page is a page.
+is_single(): Determines if the current page is a single post.
+is_category(): Determines if the current page is a category archive.
+is_tag(): Determines if the current page is a tag archive.
+is_author(): Determines if the current page is an author archive.
+is_search(): Determines if the current page is a search results page.
+is_404(): Determines if the current page is a 404 error page.
+
+*/
+?>
