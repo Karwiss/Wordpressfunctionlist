beabbb5d18cf21615705168961a9e9a11930746f
Karwiss <100564647+Karwiss@users.noreply.github.com>
2023-05-04 10:58:52 +0530

    Update wordpressfunctionlist.php

--- wordpressfunctionlist.php
+++ wordpressfunctionlist.php
@@ -44,6 +44,155 @@ is_tag(): Determines if the current page is a tag archive.
 is_author(): Determines if the current page is an author archive.
 is_search(): Determines if the current page is a search results page.
 is_404(): Determines if the current page is a 404 error page.
+get_template_part(): Retrieves and includes a template file from the current theme.
+wp_nav_menu(): Displays a custom navigation menu on the website.
+get_permalink(): Retrieves the permalink of a post or page.
+get_search_form(): Retrieves the search form template of the current theme.
+wp_enqueue_script(): Registers and enqueues a script for use on the website.
+wp_enqueue_style(): Registers and enqueues a stylesheet for use on the website.
+the_time(): Displays the date and time of a post or page.
+the_tags(): Displays the tags of a post or page.
+get_header(): Retrieves the header template of the current theme.
+get_footer(): Retrieves the footer template of the current theme.
+get_sidebar(): Retrieves the sidebar template of the current theme.
+the_title(): Displays the title of a post or page.
+the_content(): Displays the content of a post or page.
+the_excerpt(): Displays the excerpt of a post or page.
+get_the_post_thumbnail(): Retrieves the featured image of a post or page.
+the_permalink(): Displays the permalink of a post or page.
+the_author(): Displays the author name of a post or page.
+the_category(): Displays the category of a post or page.
+
+get_comment(): Retrieves a comment object for a specified comment.
+get_comments(): Retrieves an array of comment objects based on specified criteria.
+get_current_user_id(): Retrieves the ID of the currently logged-in user.
+get_userdata(): Retrieves user data for a specified user ID.
+wp_get_current_user(): Retrieves the current user object.
+wp_get_attachment_url(): Retrieves the URL of an attachment.
+wp_get_attachment_image(): Retrieves an HTML image element for an attachment.
+wp_get_attachment_metadata(): Retrieves metadata for an attachment.
+wp_get_attachment_thumb_url(): Retrieves the URL of an attachment thumbnail.
+wp_get_attachment_image_src(): Retrieves the source data for an attachment image.
+wp_get_attachment_caption(): Retrieves the caption for an attachment.
+wp_get_attachment_url(): Retrieves the URL of an attachment.
+get_the_author_meta(): Retrieves user meta data for the author of a post or page.
+get_comment_author_meta(): Retrieves user meta data for the author of a comment.
+get_header_image(): Retrieves the header image URL for the current theme.
+get_nav_menu_locations(): Retrieves the registered nav menu locations for the current theme.
+get_post_format(): Retrieves the post format for a post.
+get_post_type(): Retrieves the post type for a post.
+get_query_var(): Retrieves the value of a query variable.
+get_search_query(): Retrieves the search query string.
+get_stylesheet_directory_uri(): Retrieves the URI of the current stylesheet directory.
+get_the_category_list(): Retrieves a formatted list of categories for a post.
+get_the_date(): Retrieves the date of a post or page.
+get_the_excerpt(): Retrieves the excerpt of a post or page.
+get_the_post_thumbnail_url(): Retrieves the URL of a post thumbnail.
+get_the_terms(): Retrieves the terms associated with a post.
+has_post_thumbnail(): Determines if a post has a featured image.
+is_archive(): Determines if the current page is an archive page.
+is_attachment(): Determines if the current page is an attachment page.
+is_customize_preview(): Determines if the current page is a customize preview page.
+is_day(): Determines if the current page is a day archive.
+is_feed(): Determines if the current page is a feed.
+is_month(): Determines if the current page is a month archive.
+is_multisite(): Determines if the website is part of a multisite network.
+is_network_admin(): Determines if the current user is a network administrator.
+is_plugin_active(): Determines if a plugin is active.
+is_post_type_archive(): Determines if the current page is a post type archive.
+is_preview(): Determines if the current page is a preview.
+is_singular(): Determines if the current page is a singular post or page.
+is_super_admin(): Determines if the current user is a super admin on a multisite network.
+is_taxonomy(): Determines if the current page is a taxonomy archive.
+is_user_logged_in(): Determines if the current user is logged in.
+register_nav_menu(): Registers a navigation menu for the current theme.
+register_post_type(): Registers a custom post type for the current theme or plugin.
+register_sidebar(): Registers a widgetized sidebar for the current theme.
+sanitize_text_field(): Sanitizes a string or array of strings for safe use in the database or in output.
+the_author(): Displays the author name for a post or page.
+the_category(): Displays the categories for a post or page.
+the_content(): Displays the content of a post or page.
+the_excerpt(): Displays the excerpt of a post or page.
+the_ID(): Displays the ID of a post or page.
+the_permalink(): Displays the URL of a post or page.
+the_post(): Sets up the current post in the WordPress loop.
+the_post_thumbnail(): Displays the featured image of a post.
+the_tags(): Displays the tags for a post or page.
+the_time(): Displays the time of a post or page.
+the_title(): Displays the title of a post or page.
+update_option(): Updates the value of an option in the database.
+update_post_meta(): Updates the value of a custom field for a post or page.
+wp_enqueue_script(): Enqueues a script for use in WordPress.
+wp_enqueue_style(): Enqueues a stylesheet for use in WordPress.
+wp_footer(): Displays the contents of the footer section for the current theme.
+wp_head(): Displays the contents of the head section for the current theme.
+wp_insert_post(): Inserts a new post or page into the database.
+wp_login_url(): Retrieves the URL of the login page.
+wp_logout_url(): Retrieves the URL of the logout page.
+wp_redirect(): Redirects the user to a specified URL.
+wp_register_script(): Registers a script for use in WordPress.
+wp_register_style(): Registers a stylesheet for use in WordPress.
+wp_set_post_terms(): Sets the terms for a post or page.
+wp_title(): Displays the title of a post or page.
+get_comments_number(): Retrieves the number of comments for a post.
+get_template_part(): Loads a template part into a template.
+wp_get_post_terms(): Retrieves the terms associated with a post.
+add_action(): Adds a function to an action hook.
+add_filter(): Adds a function to a filter hook.
+apply_filters(): Applies a filter to a value.
+do_action(): Executes the functions hooked to an action hook.
+esc_html(): Escapes a string for safe use in HTML output.
+esc_url(): Escapes a URL for safe use in HTML output.
+get_ancestors(): Retrieves the ancestors of a post or term.
+get_bloginfo(): Retrieves information about the current site or blog.
+get_boundary_post(): Retrieves the previous or next post in relation to the current post.
+get_footer(): Retrieves the contents of the footer section for the current theme.
+get_header(): Retrieves the contents of the header section for the current theme.
+get_locale(): Retrieves the current locale for the site.
+get_next_posts_link(): Retrieves the link for
+get_post(): Retrieves a post object for a given post ID.
+get_post_meta(): Retrieves the value of a custom field for a post or page.
+get_posts(): Retrieves an array of post objects based on query parameters.
+get_sidebar(): Retrieves the contents of a widgetized sidebar for the current theme.
+get_template_directory_uri(): Retrieves the URL of the current theme's directory.
+get_template_part(): Loads a template part into a template.
+get_the_author_meta(): Retrieves the value of a custom user meta field for the current author.
+get_the_category(): Retrieves the category object or objects for a post.
+get_the_date(): Retrieves the date of a post.
+get_the_excerpt(): Retrieves the excerpt of a post or page.
+get_the_post_thumbnail(): Retrieves the featured image of a post.
+get_the_tags(): Retrieves the tags for a post or page.
+get_the_time(): Retrieves the time of a post or page.
+get_the_title(): Retrieves the title of a post or page.
+has_post_thumbnail(): Determines if a post has a featured image.
+is_active_sidebar(): Determines if a sidebar is active for the current theme.
+is_page(): Determines if the current page is a page.
+is_single(): Determines if the current page is a single post.
+is_user_logged_in(): Determines if the current user is logged in.
+remove_action(): Removes a function from an action hook.
+remove_filter(): Removes a function from a filter hook.
+sanitize_email(): Sanitizes an email address for safe use in the database or in output.
+sanitize_file_name(): Sanitizes a filename for safe use on the filesystem.
+sanitize_title(): Sanitizes a string for use as a post or page title.
+the_author_meta(): Displays the value of a custom user meta field for the current author.
+the_category(): Displays the categories for a post or page.
+the_date(): Displays the date of a post or page.
+the_excerpt(): Displays the excerpt of a post or page.
+the_post_thumbnail_url(): Displays the URL of the featured image of a post.
+the_tags(): Displays the tags for a post or page.
+wp_count_posts(): Retrieves a count of posts for each post status.
+wp_create_nonce(): Generates a unique nonce for use in forms and URLs.
+wp_delete_post(): Deletes a post or page from the database.
+wp_deregister_script(): Deregisters a script from use in WordPress.
+wp_get_attachment_image(): Retrieves an image HTML element for a given attachment ID.
+wp_get_attachment_image_src(): Retrieves the URL, width, and height of an image for a given attachment ID.
+wp_get_attachment_metadata(): Retrieves metadata for a given attachment ID.
+wp_get_current_user(): Retrieves the current user object.
+wp_get_nav_menu_items(): Retrieves an array of menu items for a given menu.
+wp_insert_attachment(): Inserts an attachment into the database.
+wp_insert_category(): Inserts a new category into the database.
+wp_login_form(): Displays
+
 
 */
 ?>
